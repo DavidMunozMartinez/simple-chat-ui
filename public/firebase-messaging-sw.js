b0c85e3cd7639cbb42755b8a567a2239c9987e1c
DavidMunozMartinez <jdavid_9502@outlook.com>
2022-12-28 14:01:53 -0600

    notification grouping and accessibility changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,7 +12,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-messaging.onBackgroundMessage(messaging, (payload) => {
+
+messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
   if (payload.notification) {
@@ -22,10 +23,32 @@ messaging.onBackgroundMessage(messaging, (payload) => {
     const notificationOptions = {
       body: notification.body,
       icon: notification.icon,
-      image: notification.image
+      image: notification.image,
+      tag: payload.data.tag
     };
   
     self.registration.showNotification(notificationTitle,
       notificationOptions);
   }
+});
+
+self.addEventListener('notificationclick', function(event) {
+  let url = 'https://simple-chat-ui.vercel.app/';
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+      clients.matchAll({type: 'window'}).then( windowClients => {
+          // Check if there is already a window/tab open with the target URL
+          for (var i = 0; i < windowClients.length; i++) {
+              var client = windowClients[i];
+              // If so, just focus it.
+              if (client.url === url && 'focus' in client) {
+                  return client.focus();
+              }
+          }
+          // If not, then open the target URL in a new window/tab.
+          if (clients.openWindow) {
+              return clients.openWindow(url);
+          }
+      })
+  );
 });
